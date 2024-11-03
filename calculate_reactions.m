5837292a9956b72876b4aa7f54ed6e69933403b9
Max <max_choudhury@yahoo.com>
2024-11-03 01:02:31 +0000

    Displacement calculations as well as vertical calculatiosn for the point mass at C (top of pylon above sea level). The displacements are not very correct as this our moment equations have not accounted for much beam bending as of yet.

--- calculate_reactions.m
+++ calculate_reactions.m
@@ -1,21 +1,23 @@
-function [H_a, V_a, M_a] = calculate_reactions(Fthrust, Fwave, Fwind, H, h)
+function [H_a, V_a, M_a] = calculate_reactions(Fthrust, Fwave, Fwind, weight_C, H, h)
     % Total Length of the Tower
     L = H + h; % Total length of the tower
 
     % Heights at which forces are applied
     height_fwave = h;                  % Fwave acts at height h (below sea level)
     height_fwind = (H / 2) + h;        % Fwind acts halfway between the top of the pylon and sea level
+    height_C = L;                       % Weight of the mass acts at height L (top of the tower)
 
     % Calculate moment about point A due to applied forces
     M_fwave = Fwave * height_fwave;   % Moment from wave loading
     M_fwind = Fwind * height_fwind;    % Moment from wind loading
+    M_weight_C = weight_C * height_C;  % Moment from the weight at point C
 
     % Total Moment at point A
-    M_a = Fthrust * L + M_fwave + M_fwind; % Total moment at A
+    M_a = Fthrust * L + M_fwave + M_fwind + M_weight_C; % Total moment at A
 
     % Horizontal reaction at A
     H_a = Fthrust + Fwave + Fwind;  % Sum of thrust, wave, and wind forces in the x direction
 
-    % Vertical reaction at A (still assumed to be zero)
-    V_a = 0;   
+    % Vertical reaction at A (considering weight of mass C)
+    V_a = weight_C;   % Vertical reaction is equal to the weight at point C
 end
--- offshore_tower_model.m
+++ offshore_tower_model.m
@@ -6,25 +6,38 @@ clear; clc;
 
 %% Define Parameters
 % Thrust force (set to numeric value)
-Fthrust = 1000;  % Thrust force in Newtons (numeric value)
+Fthrust = 100;  % Thrust force in kN (numeric value)
 % Wave loading (input as needed)
-Fwave = 500;     % Wave force in Newtons (numeric value)
+Fwave = 10;     % Wave force in kN (numeric value)
 % Wind loading (input as needed)
-Fwind = 600;     % Wind force in Newtons (numeric value; can be modified)
+Fwind = 8;     % Wind force in kN (numeric value; can be modified)
 
 % Height above sea level
 H = 80;          % Height above sea level in meters (numeric value)
 % Depth below sea level
 h = 20;          % Depth below sea level in meters (numeric value)
 
-% Define symbolic variables for other parameters
-syms E I m % E: Young's modulus, I: Moment of inertia, m: mass
+% Define mass at point C
+mass_C = 10;  % Mass in tonnes
+g = 9.81;     % Acceleration due to gravity in m/s^2
+weight_C = mass_C * 1000 * g;  % Convert to Newtons (N)
 
-%% Call the calculation function
-[H_a, V_a, M_a] = calculate_reactions(Fthrust, Fwave, Fwind, H, h);
+% Material properties (define as needed)
+E = 2.1e11;     % Young's modulus in Pascals (for steel, for example)
+I = calculate_tower_properties();       % Moment of inertia in m^4 for the tower cross-section
+
+%% Call the calculation function for reactions
+[H_a, V_a, M_a] = calculate_reactions(Fthrust * 1000, Fwave * 1000, Fwind * 1000, weight_C, H, h);  % Convert kN to N
+
+%% Call the displacement calculation function
+[x_b, x_c] = calculate_displacements(H, h, Fthrust * 1000, Fwave * 1000, Fwind * 1000, weight_C, E, I);  % Convert kN to N
 
 %% Display Results
 fprintf('Reaction forces and moments at the seabed (Point A):\n');
-fprintf('Horizontal reaction force (H_a): %g N\n', double(H_a));
-fprintf('Vertical reaction force (V_a): %g N\n', double(V_a)); 
-fprintf('Moment reaction (M_a): %g Nm\n', double(M_a));
+fprintf('Horizontal reaction force (H_a): %g kN\n', double(H_a) / 1000);  % Convert N to kN for display
+fprintf('Vertical reaction force (V_a): %g kN\n', double(V_a) / 1000);  % Convert N to kN for display
+fprintf('Moment reaction (M_a): %g kNm\n', double(M_a) / 1000);  % Convert Nm to kNm for display
+
+fprintf('Displacement at sea level (Point B): %g m\n', x_b);
+fprintf('Displacement at the top of the pylon (Point C): %g m\n', x_c);
+
